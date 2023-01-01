31ba6fe7a4c8425aa2fe82430401e7650938bd95
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-01 15:35:00 -0300

    * endpoint findall para user

--- src/main/java/br/com/dicasdeumdev/api/services/Impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/api/services/Impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import br.com.dicasdeumdev.api.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,9 +15,15 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository repository;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Usuario nao foi encontrado"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
     }
 }
