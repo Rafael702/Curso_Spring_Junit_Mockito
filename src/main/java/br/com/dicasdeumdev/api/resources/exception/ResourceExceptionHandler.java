44e7ff8d3c0602c76e1e2a18bcebee22adb1a5e9
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-01 15:13:30 -0300

    * Criando recurso para retorno de exception

---
+++ src/main/java/br/com/dicasdeumdev/api/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.com.dicasdeumdev.api.resources.exception;
+
+import br.com.dicasdeumdev.api.services.exception.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException exception, HttpServletRequest httpError) {
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), exception.getMessage(), httpError.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
