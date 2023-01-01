62a96941bdea7e50d6128c735049f7f2319ab227
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-01 15:11:42 -0300

    * Criando nome da tabela para entidade

--- src/main/java/br/com/dicasdeumdev/api/domain/User.java
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ import lombok.NoArgsConstructor;
 //@EqualsAndHashCode
 //@ToString
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
