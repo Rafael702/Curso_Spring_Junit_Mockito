d4024f37e32a8efc13a00dcc6e0291ccd2f8153a
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-01 14:00:50 -0300

    * Criando classe domain User

---
+++ src/main/java/br/com/dicasdeumdev/api/domain/User.java
@@ -0,0 +1,29 @@
+package br.com.dicasdeumdev.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//@EqualsAndHashCode
+//@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
