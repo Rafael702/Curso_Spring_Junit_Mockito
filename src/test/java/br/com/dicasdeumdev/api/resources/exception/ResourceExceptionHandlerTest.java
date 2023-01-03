a50c231bbc11327ea18acc1e6003a820419b4160
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-02 23:33:40 -0300

    * Teste para manipulador de exception no metodo data_integrity_violation

--- src/test/java/br/com/dicasdeumdev/api/resources/exception/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resources/exception/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.resources.exception;
 
+import br.com.dicasdeumdev.api.services.exception.DataIntegrityViolationException;
 import br.com.dicasdeumdev.api.services.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @SpringBootTest
 class ResourceExceptionHandlerTest {
     public static final String USUARIO_NAO_FOI_ENCONTRADO = "Usuario nao foi encontrado";
+    public static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail ja cadastrado no sistema";
 
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
@@ -41,5 +43,16 @@ class ResourceExceptionHandlerTest {
 
     @Test
     void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegrityViolationException(new DataIntegrityViolationException(E_MAIL_JA_CADASTRADO_NO_SISTEMA),
+                new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_JA_CADASTRADO_NO_SISTEMA, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
