f24bae3cf72d371635ce16d0506d51b83741772d
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-02 22:19:18 -0300

    * Teste para metodo delete com sucesso

--- src/test/java/br/com/dicasdeumdev/api/services/Impl/UserServiceImplTest.java
+++ src/test/java/br/com/dicasdeumdev/api/services/Impl/UserServiceImplTest.java
@@ -21,6 +21,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -143,7 +146,13 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void mustDeleteUserWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+
+        service.delete(ID);
+
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
     private void startUser() {
